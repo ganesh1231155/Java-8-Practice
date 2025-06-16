20dd65282d2c5f68538dbf502b120ff53253264f
ganesh1231155 <kolteshreeganesh1155@gmail.com>
2025-06-16 18:08:48 +0530

    Predicate Functional Interface to check if a number is even or odd

---
+++ src/main/java/org/study/FunctionalInterfaces/EvenOrOdd.java
@@ -0,0 +1,12 @@
+package org.study.FunctionalInterfaces;
+
+interface Predicate<T>{
+    boolean check(T t);
+}
+
+public class EvenOrOdd {
+    public static void main(String sk[]) {
+        Predicate<Double> evenOdd = num -> num % 2 == 0;
+        System.out.println(evenOdd.check((double)1)?"Number is Even.":"Number is Odd.");
+    }
+}
