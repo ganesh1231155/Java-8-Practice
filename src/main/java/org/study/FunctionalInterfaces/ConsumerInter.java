1bb8ef27ea1d07b23502ebcb44d75662adef8f19
ganesh1231155 <kolteshreeganesh1155@gmail.com>
2025-06-17 17:54:06 +0530

    Consumer Interface to generate Square

--- src/main/java/org/study/FunctionalInterfaces/ConsumerInter.java
+++ src/main/java/org/study/FunctionalInterfaces/ConsumerInter.java
@@ -4,9 +4,17 @@ package org.study.FunctionalInterfaces;
 interface consumer<T>{
     void print(T t);
 }
+
+@FunctionalInterface
+interface mathSquare<T>{
+    void print(T t);
+}
 public class ConsumerInter {
     public static void main(String sk[]){
         consumer<String> messanger=str-> System.out.println(str);
         messanger.print("Hello World!");
+
+        mathSquare<Double> mathSqr=num->System.out.println(num+"*"+num+"="+num*num);
+        mathSqr.print(12.0);
     }
 }
